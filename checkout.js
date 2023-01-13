99ab18345fec8ee34abacf62bb8b67caba7715c6
morpline <118403086+morpline@users.noreply.github.com>
2023-01-13 13:21:56 -0700

    Finished the checkout kata

--- checkout.js
+++ checkout.js
@@ -1,7 +1,8 @@
 module.exports = class Checkout{
     constructor(){
         this.prices = new Object();
-        this.total = 0;
+        this.discounts = new Object();
+        this.items = new Object();
     }
 
     addItemPrice(item,price){
@@ -9,9 +10,38 @@ module.exports = class Checkout{
     }
 
     addItem(item){
-        this.total+= this.prices[item];
+        if(this.prices[item] == undefined) {
+            throw("No price defined for item "+item);
+        }
+        // this.total+= this.prices[item];
+        if(this.items[item] == undefined)
+        {
+            this.items[item] = 1;
+        } else {
+            this.items[item]++;
+        }
     }
     calculateTotal(){
-        return this.total;
+        let total = 0;
+        for(var item in this.items){
+            total += this.calculateItemTotal(item);
+        }
+        return total;
+    }
+    calculateItemTotal(item){
+        let total = 0;
+        let discount = this.discounts[item];
+        if(discount != undefined){
+            let nbrOfDiscounts = this.items[item] / discount.cnt;
+            total += nbrOfDiscounts * discount.price;
+            let remainder = this.items[item] % discount.cnt;
+            total += remainder * this.prices[item];
+        } else {
+            total += (this.prices[item] * this.items[item]);
+        }
+        return total;
+    }
+    addDiscount(item, itemCnt, discountPrice){
+        this.discounts[item] = {cnt:itemCnt, price:discountPrice};
     }
 }
--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -4,10 +4,29 @@ var checkout;
 
 beforeEach(function(){
     checkout = new Checkout();
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
 })
 
 it("can calculate the current total", function(){
-    checkout.addItemPrice("a", 1);
-    checkout.addItem("a");
-    expect(checkout.calculateTotal()).to.equal(1);
+    checkout.addItem("a")
+    expect(checkout.calculateTotal()).to.equal(1)
+})
+it("can add multiple items and get correct total", function(){
+    checkout.addItem("a")
+    checkout.addItem("b")
+    expect(checkout.calculateTotal()).to.equal(3)
+})
+it("Can add discount rule", function(){
+    checkout.addDiscount("a",3,2)
+})
+it("can apply discount rules to the total",function(){
+    checkout.addDiscount("a",3,2)
+    checkout.addItem('a');
+    checkout.addItem('a');
+    checkout.addItem('a');
+    expect(checkout.calculateTotal()).to.equal(2)
+})
+it("Throws when item added has no price defined", function(){
+    expect(function(){checkout.addItem("c")}).to.throw();
 })
